914319dd8bfb4168b3c69eca017c4322434cbddc
Peter Omollo <omollpeter@gmail.com>
2024-08-10 16:22:27 +0300

    Fix index to be on the first letter

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,4 @@
 -- This SQL script that creates an index idx_name_first on the table names 
 -- and the first letter of name
 
-DROP INDEX IF EXISTS idx_name_first ON names;
-
-CREATE UNIQUE INDEX idx_name_first ON names(name(1));
+CREATE INDEX idx_name_first ON names(name(1));

36a8f4c3b6c87b9588a74e7d0aa98523cc164ed1
Peter Omollo <omollpeter@gmail.com>
2024-08-15 21:34:56 +0300

    Implement a decorator to track the number of calls made to Cache redis instance

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,19 @@ Contains class definitions for Cache instance of Redis
 import redis
 from typing import Union, Callable
 from uuid import uuid4
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """
+    A decorator to count all the 
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        method_name = method.__qualname__
+        self._redis.incr(method_name)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     """
@@ -19,6 +31,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Generates a key, store input data using the key in Redis server
@@ -28,6 +41,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    @count_calls
     def get(
             self, key: str, fn: Callable = None
         ) -> Union[bytes, str, int, float]:
@@ -40,10 +54,12 @@ class Cache:
                 return fn(value)
         return value
     
+    @count_calls
     def get_str(self, key: str) -> Union[str, bytes]:
         """Returns string value of a particular key"""
         return self.get(key, str)
 
+    @count_calls
     def get_int(self, key: str) -> Union[int, bytes]:
         """Returns integer value of a particular key"""
         return self.get(key, int)

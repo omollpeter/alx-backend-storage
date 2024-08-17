71f81523e3324957b1b152c6810676c5236436af
Peter Omollo <omollpeter@gmail.com>
2024-08-17 05:20:18 +0300

    Fix 101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -9,18 +9,6 @@ def top_students(mongo_collection):
     """
     Returns all students sorted by average score
     """
-    # mongo_collection.delete_many({})
-    # students = mongo_collection.find()
-    # for student in students:
-    #     totalScore = 0
-    #     for topic in student.get("topics"):
-    #         totalScore += topic.get("score")
-    #     averageScore = totalScore / len(student.get("topics"))
-    #     mongo_collection.update_one(
-    #         {"name": student.get("name")},
-    #         {"$set": {"averageScore": averageScore}}
-    #     )
-    # return mongo_collection.find().sort({"averageScore": -1})
     pipeline = [
         {
             "$project": {

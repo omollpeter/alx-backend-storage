02f72f83721b7fa59963166c95153c518866bd21
Peter Omollo <omollpeter@gmail.com>
2024-08-17 04:42:26 +0300

    Write a Python function that returns all students sorted by average score:

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,3 +4,19 @@ This module contains a function that returns all students sorted by
 average score
 """
 
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score
+    """
+    students = mongo_collection.find()
+    for student in students:
+        totalScore = 0
+        for topic in student.get("topics"):
+            totalScore += topic.get("score")
+        averageScore = totalScore / len(student.get("topics"))
+        mongo_collection.update_one(
+            {"name": student.get("name")},
+            {"$set": {"averageScore": averageScore}}
+        )
+    return mongo_collection.find().sort({"averageScore": -1})
